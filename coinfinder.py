453a3d21df930aa8e26401b518f61c8a4c311d86
BarrsX <xaionbarrs@gmail.com>
2021-02-11 18:34:04 -0500

    First Commit

---
+++ coinfinder.py
@@ -0,0 +1,49 @@
+import json
+import requests
+from requests import Session
+from requests import Session
+api_key = '0ed0eb04-94e3-4ce1-aa32-cbac4f8702f1'
+global_url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
+
+
+def coin_finder():
+    print('\nEnter coin symbol')
+    coin = input()
+    coin = coin.upper()
+
+    parameters = {
+        'convert': 'USD',
+        'symbol': coin
+    }
+
+    headers = {
+        'Accepts': 'application/json',
+        'X-CMC_PRO_API_KEY': api_key,
+    }
+
+    session = Session()
+    session.headers.update(headers)
+
+    response = session.get(global_url, params=parameters)
+    data = json.loads(response.text)
+
+    name = data['data'][coin]['name']
+    coin_price = data['data'][coin]['quote']['USD']['price']
+    coin_price = round(coin_price, 4)
+    coin_day_change = data['data'][coin]['quote']['USD']['percent_change_24h']
+    coin_day_change = str(round(coin_day_change, 2)) + '%'
+    volume = data['data'][coin]['quote']['USD']['volume_24h']
+    volume = round(volume, 2)
+
+    print(name)
+    print('price: ' + '$' + str(coin_price))
+    print('change in last day: ' + str(coin_day_change))
+    print('volume in last day: ' + str(volume))
+
+
+while True:
+    coin_finder()
+    print('Run again? (y/n)')
+    leave = input()
+    if leave == 'n':
+        break
---
+++ portfolio.py
@@ -0,0 +1,59 @@
+import requests
+import json
+import prettytable
+from requests import Session
+from prettytable import PrettyTable
+
+x = PrettyTable()
+
+api_key = '0ed0eb04-94e3-4ce1-aa32-cbac4f8702f1'
+global_url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
+
+parameters = {
+    'convert': 'USD',
+    'symbol': ''
+}
+
+headers = {
+    'Accepts': 'application/json',
+    'X-CMC_PRO_API_KEY': api_key,
+}
+
+x.field_names = ["Asset", "Amount", "Value Owned", "Price", "1H", "24H", '7D']
+
+
+def get_coin(coin, amount):
+    parameters['symbol'] = coin
+
+    session = Session()
+    session.headers.update(headers)
+
+    response = session.get(global_url, params=parameters)
+    data = json.loads(response.text)
+
+    coin_price = data['data'][coin]['quote']['USD']['price']
+    coin_value = amount * coin_price
+    coin_hour_change = round(data['data'][coin]['quote']['USD']['percent_change_1h'], 2)
+    coin_day_change = round(data['data'][coin]['quote']['USD']['percent_change_24h'], 2)
+    coin_week_change = round(data['data'][coin]['quote']['USD']['percent_change_7d'], 2)
+    x.add_rows(
+        [
+            [coin, amount, '$' + str(coin_value), '$' + str(coin_price), coin_hour_change, coin_day_change,
+             str(coin_week_change) + '%'],
+        ]
+    )
+
+
+ans = 'y'
+while ans == 'y':
+    print('\nEnter coin symbol')
+    sym = input()
+    sym = sym.upper()
+    print('Enter amount')
+    am = input()
+    am = float(am)
+    get_coin(sym, am)
+    print(x)
+    print('Add another coin? (y/n)')
+    ans = input()
+
